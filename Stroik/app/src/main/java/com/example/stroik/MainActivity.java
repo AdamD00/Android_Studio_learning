c38b55f49ee287929551e3511adfe3ce2706f1f5
Mrokon <mrokon00@gmail.com>
2022-12-03 11:06:19 +0100

    Skonfigurowanie clicku oraz menu gestowego

--- Stroik/app/src/main/java/com/example/stroik/MainActivity.java
+++ Stroik/app/src/main/java/com/example/stroik/MainActivity.java
@@ -3,57 +3,138 @@ package com.example.stroik;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.dynamicanimation.animation.DynamicAnimation;
-import androidx.dynamicanimation.animation.FlingAnimation;
 
-import android.graphics.drawable.shapes.OvalShape;
-import android.graphics.drawable.shapes.Shape;
-import android.util.Log;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import android.os.Bundle;
 
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
+public class MainActivity extends AppCompatActivity {
    private GestureDetector mDetector;
    private static final String TAG = "Swipe Position";
-   private float x1,x2,y1,y2;
+   private float xDown, xDefault, distanceX;
    private int i = 0;
-   private static int MIN_DISTANCE = 150;
+   private static int MIN_DISTANCE = 100;
+   private static int MAX_DISTANCE = 700;
    private int[] tabImg ={
            R.drawable.violin,
            R.drawable.cello,
            R.drawable.guitar
    };
+
    private String[] tabString={
            "Skrzypce",
            "Wiolonczela",
            "Gitara"
    };
-   private ImageView view;
+   private ImageButton btn;
+   private ImageView view, instrument;
    private TextView txtView;
+   private RelativeLayout linLay;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-         view = findViewById(R.id.imageView);
-         txtView = findViewById(R.id.textView3);
-        this.mDetector = new GestureDetector(MainActivity.this,this);
-        view.setImageResource(tabImg[i]);
-        txtView.setText(tabString[i]);
-
+    public MainActivity() {
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         txtView = findViewById(R.id.textView3);
+         linLay = findViewById(R.id.layout);
+       // view.setImageResource(tabImg[i]);
+        txtView.setText(tabString[i]);
+        btn = findViewById(R.id.imageButton1);
+
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch(event.getActionMasked()){
+                    case MotionEvent.ACTION_DOWN:
+                        xDown = event.getX();
+                       xDefault = btn.getX();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        float moveX;
+                        moveX = event.getX();
+                         distanceX=moveX-xDown;
+                        float transformation = btn.getX()+distanceX;
+                        if((Math.abs(xDefault-transformation)>MIN_DISTANCE) && (Math.abs(xDefault-transformation)<MAX_DISTANCE)) btn.setX(btn.getX()+distanceX);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if(distanceX>0)
+                        {
+                            if(i==2) i=-1;
+                            i++;
+
+                        }
+                        else if(distanceX<0)
+                        {
+                            if(i==0) i=3;
+                            i--;
+                        }
+                        else {
+                            switch (i) {
+                                case 0:
+                                    Toast.makeText(getApplicationContext(), "Stroimy skrzypce", Toast.LENGTH_SHORT).show();
+                                    break;
+                                case 1:
+                                    Toast.makeText(getApplicationContext(), "Stroimy wiolke", Toast.LENGTH_SHORT).show();
+                                    break;
+                                case 2:
+                                    Toast.makeText(getApplicationContext(), "Stroimy gitarę", Toast.LENGTH_SHORT).show();
+                                    break;
+                            }
+                        }
+                        distanceX = 0;
+                        btn.setImageResource(tabImg[i]);
+                        txtView.setText(tabString[i]);
+                       btn.setX(xDefault);
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+    }
+    public void onClick (ImageButton btn)
+    {
+
+        switch(i)
+        {
+            case 0:
+                Toast.makeText(getApplicationContext(), "Stroimy skrzypce", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(getApplicationContext(), "Stroimy wiolke", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(getApplicationContext(), "Stroimy gitarę", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+
+/*
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        mDetector.onTouchEvent(event);
+
 
         switch(event.getAction()){
             case MotionEvent.ACTION_DOWN:
@@ -71,12 +152,29 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
                     if(x2>x1)
                     {
+                    float x = view.getTranslationX();
                         if(i==2) i=-1;
                         i++;
+                        AnimatorSet w = new AnimatorSet();
+                        AnimatorSet s = new AnimatorSet();
+                        s.playSequentially(ObjectAnimator.ofFloat(view,"translationX",1000));
+                        s.setDuration(800);
+                        s.start();
+                        view.setTranslationX(-1000);
+
+                        w.playSequentially(ObjectAnimator.ofFloat(view,"translationX",50));
+                        w.setDuration(800);
+                        w.start();
+
+
 
                     }
                     else
                     {
+                        view.animate()
+                                .translationX(0)
+                                .setDuration(800)
+                                .start();
                         if(i==0) i=3;
                         i--;
                     }
@@ -118,6 +216,5 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     @Override
     public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        return false;
+        return false;*/
     }
-}
